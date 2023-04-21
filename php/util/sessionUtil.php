34ef66df656494ba5b5b48a4fb1c31e841575313
Paolo Palumbo <paolo@iMac.station>
2023-04-21 17:10:23 +0200

    importate delle util

---
+++ php/util/sessionUtil.php
@@ -0,0 +1,17 @@
+<?php
+	
+	//setSession: set $_SESSION properly
+	function setSession($username, $userId){
+		$_SESSION['userId'] = $userId;
+		$_SESSION['username'] = $username;
+	}
+
+	//isLogged: check if user has logged in and, if it is the case, returns the username
+	function isLogged(){		
+		if(isset($_SESSION['userId']))
+			return $_SESSION['userId'];
+		else
+			return false;
+	}
+
+?>
