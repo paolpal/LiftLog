7cdca35028516df74627418998ee62dc4d1026d8
Paolo Palumbo <paolo@iMac.station>
2023-04-21 16:53:53 +0200

    inzio homepage

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Paolo Palumbo">
+    </head>
+    <body>
+        <header>
+            <h1>LiftLog</h1>
+        </header>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Orario</a>
+            <a href="">Chi siamo</a>
+            <a href="">Login</a>
+        </nav>
+        <section></section>
+    </body>
+</html>
