23fcbe9b171ad34c058ba15ff0d1c35af67cd4b9
paolo-asus <paol.palumbo@gmail.com>
2023-06-03 12:30:48 +0200

    Corretti path relativi nella navbar

--- php/config.php
+++ php/config.php
@@ -4,4 +4,6 @@
 	define ("DIR_UTIL", DIR_BASE . 'util/');
 	define ("DIR_LAYOUT", DIR_BASE . 'layout/');
 	define ("DIR_AJAX_UTIL", DIR_BASE . 'ajax/');
+	define("ROOT_DIR", realpath(__DIR__.'/..').'/');
+	define("ROOT_PATH", '/' . basename(rtrim(ROOT_DIR, '/\\')) . '/');
 ?>
--- php/layout/navbar.php
+++ php/layout/navbar.php
@@ -4,14 +4,14 @@
 	include_once DIR_UTIL."sessionUtil.php";
 ?>
 <nav>
-	<a href="/LiftLog/index.php">Home</a>
-	<a href="/LiftLog/index.php#chisiamo">Chi siamo</a>
-	<a href="/LiftLog/index.php#motivazione">La motivazione</a>
-	<a href="/LiftLog/index.php#dovetrovarci">Dove trovarci</a>
+	<a href=<?php echo ROOT_PATH . "index.php"?>>Home</a>
+	<a href=<?php echo ROOT_PATH . "index.php#chisiamo"?>>Chi siamo</a>
+	<a href=<?php echo ROOT_PATH . "index.php#motivazione"?>>La motivazione</a>
+	<a href=<?php echo ROOT_PATH . "index.php#dovetrovarci"?>>Dove trovarci</a>
 	<?php
 	if(isLogged()){
-		echo '<a href="/LiftLog/php/esercizi.php">Parte privata</a>';
-		echo '<a href="/LiftLog/php/util/logout.php" class="split">Logout</a>';
+		echo '<a href="' . ROOT_PATH . 'php/esercizi.php">Parte privata</a>';
+		echo '<a href="' . ROOT_PATH . 'php/util/logout.php" class="split">Logout</a>';
 	}
 	else{
 		echo '<a onclick="open_login_form()" class="split">Login</a>';
